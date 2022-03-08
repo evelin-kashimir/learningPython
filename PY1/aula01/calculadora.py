26482f1ef5edae282565d932042002bf4ab74bf4
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-08 20:02:14 -0300

    Fundamentos: Exercicio calculadora

---
+++ PY1/aula01/calculadora.py
@@ -0,0 +1,19 @@
+print('---- CALCULADORA INTELIGENTE -----')
+valor1 = int(input('Informe um numero inteiro: '))
+valor2 = int(input('Informe um outro numero inteiro: '))
+
+operacao = valor1 + valor2 
+print('SOMA')
+print(valor1," + ",valor2," = ",operacao)
+
+operacao = valor1 - valor2 
+print('SUBTRAÇÃO')
+print(valor1," - ",valor2," = ",operacao)
+
+operacao = valor1 * valor2 
+print('MULTIPLICAÇÃO')
+print(valor1," x ",valor2," = ",operacao)
+
+operacao = valor1 / valor2 
+print('DIVISÃO')
+print(valor1," / ",valor2," = ",operacao)
