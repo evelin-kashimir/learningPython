7b6e538b3ad6d7e5ad4dfa3e6809aba5947b0bfc
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-08 19:52:50 -0300

    Fundamentos: Imprimindo mensagens

---
+++ PY1/aula01/message.py
@@ -0,0 +1,17 @@
+#declarando variável
+msg = "Seja bem vindo(a) ao curso de Python!"  
+#imprime algo - saída
+print(type(msg))  
+msg = 2  
+print(type(msg))      
+msg = 1.5
+print(type(msg))     
+msg = True
+print(type(msg)) 
+
+# Entrada de dados
+nome = input('Informe o seu nome: ')
+print(nome)
+
+#del(soma) deletando variavel
+
