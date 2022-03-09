30bc9fba02635fd0e15ffc9b5a3c1231a57c84e7
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-08 21:39:52 -0300

    Fundamentos: Exercicio motorista (refatorado)

--- PY1/aula01/exercicios/motorista.py
+++ PY1/aula01/exercicios/motorista.py
@@ -2,10 +2,13 @@ nome = input('Digite seu nome: ')
 nascimento = int(input('Digite o seu ano de nascimento: '))
 
 anoAtual = 2022
+idadeMinima = 18
 idade = anoAtual - nascimento
 
-if(idade > 18):
+if(idade > idadeMinima):
     print(nome + ", você tem " + str(idade) + " anos, ESTÁ apto(a) a dirigir!")
 else:
-    print(nome + ", você tem " + str(idade) + " anos, NÃO ESTÁ apto(a) a dirigir!")
+    faltam = idadeMinima - idade
+    print(nome + ", você tem " + str(idade) + " anos, NÃO ESTÁ apto(a) a dirigir! Faltam mais " + str(faltam) + " anos.")
+
 
