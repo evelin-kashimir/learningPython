6a59ed7eb422e56c6cff99c8a5c51066133e7e83
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-10 22:13:05 -0300

    Fundamentos: Exercicio tabuada utilizando laço FOR (refatorado)

--- PY1/aula02/exercicios/tabuada.py
+++ PY1/aula02/exercicios/tabuada.py
@@ -1,3 +1,7 @@
 num = int(input("Digite um valor inteiro: "))
-for valor in range(1,11):
-    print(str(num) + " x " + str(valor) +" = "+ str(valor*num))
+
+for cont in range(num, 11):
+    for valor in range(1,11):
+        print(str(cont) + " x " + str(valor) +" = "+ str(valor*cont))
+
+
