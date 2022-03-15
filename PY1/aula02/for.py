bead0ebb3e081e4556d0d97b57896df13bb1bb6a
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-15 19:44:13 -0300

    Fundamentos: For com Break, pass e continue

--- PY1/aula02/for.py
+++ PY1/aula02/for.py
@@ -4,3 +4,16 @@ for valor in range(1,10):
 
 for valor in range(10,1):
     print(valor)
+
+
+#break
+for num in range(1, 10):
+    print(num)
+    if(num == 5):
+        break
+    
+#continue 
+for cont in range(1, 10):
+    print(cont)
+    if(num == 5):
+        continue
