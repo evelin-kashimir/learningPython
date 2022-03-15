ea7eafe29337dfad126dc8ef89dcb27b9e696109
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-15 18:56:10 -0300

    Fundamentos: Exercicio While (refatorado)

--- PY1/aula03/exercicios/while.py
+++ PY1/aula03/exercicios/while.py
@@ -1,8 +1,8 @@
 num = 1
 soma = 0
 while (num > 0):
-    num = int(input("Digite um número: "))
     soma = soma + num
-
-print('Saindo...')
+    num = int(input("Digite um número: "))
+    
 print("A soma dos numeros digitados: " + str(soma))
+print('Saindo...')
