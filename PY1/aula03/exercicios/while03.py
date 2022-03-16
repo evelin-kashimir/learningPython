84c0d78757d5d3edb1a4b14f72886b2db55ac2f3
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-15 21:29:34 -0300

    Fundamentos: Desafio While - Destribuindo notas (aprimorado)

--- PY1/aula03/exercicios/while03.py
+++ PY1/aula03/exercicios/while03.py
@@ -11,41 +11,49 @@ saque = int(input("Valor de Saque: R$"))
 while( saque >= 100 ):
     nota100+=1
     saque -= 100
+if(nota100 > 0):
+    print("Notas de 100 -> " + str(nota100))
 
 #notas 50
 while( saque >= 50 ):
     nota50+=1
     saque -= 50
+if(nota50 > 0):
+    print("Notas de 50 -> " + str(nota50))
 
 #notas 20
 while( saque >= 20 ):
     nota20+=1
     saque -= 20
+if(nota20 > 0):
+    print("Notas de 20 -> " + str(nota20))
 
 #notas 10
 while( saque >= 10 ):
     nota10+=1
     saque -= 10
+if(nota10 > 0):
+    print("Notas de 10 -> " + str(nota10))
 
 #notas 5
 while( saque >= 5):
     nota5+=1
     saque -= 5
+if(nota5 > 0):
+    print("Notas de 5 -> " + str(nota5))
 
 #notas 2
 while( saque >= 2):
     nota2+=1
     saque -= 2
+if(nota2 > 0):
+    print("Notas de 2 -> " + str(nota2))
 
 #notas 1
 while( saque >= 1 ):
     nota1+=1
     saque -= 1
+if(nota1 > 0):
+    print("Notas de 1 -> " + str(nota1))
 
-print("Notas de 100 -> " + str(nota100))
-print("Notas de 50 -> " + str(nota50))
-print("Notas de 20 -> " + str(nota20))
-print("Notas de 10 -> " + str(nota10))
-print("Notas de 5 -> " + str(nota5))
-print("Notas de 2 -> " + str(nota2))
-print("Notas de 1 -> " + str(nota1))
+
