7c045401bbee671f1df2965c1481fd8cc4fdeffb
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-15 22:00:56 -0300

    Fundamentos: Trabalhando com listas

---
+++ PY1/aula03/array.py
@@ -0,0 +1,11 @@
+lista = [5, 3, 5.5, 'oi', True]
+lista2 = ['Proway', lista]
+
+print(lista)
+print(lista[0])
+print(lista2[1])
+
+print(type(lista2[0]))
+#verificando se é algo
+if type(lista2[0]) is str:
+    print(lista2[0] + ' é uma string ')
