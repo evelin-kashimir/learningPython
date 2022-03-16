e7caf4df0af260a2cde33fc9dd0aa243c272307e
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-15 21:46:06 -0300

    Fundamentos: Tipos booleanos

---
+++ PY1/aula03/booleano.py
@@ -0,0 +1,18 @@
+if True:
+    print('Verdadeiro')
+if False:
+    print('Falso')
+
+login = 'aluno'
+senha = 'proway'
+
+if(login == 'aluno' and senha == 'proay'):
+    permite = True
+else:
+    permite = False
+
+if(permite):
+    print('Login efetuado com sucesso')
+else: 
+    print('Login ou senha incorretos')
+
