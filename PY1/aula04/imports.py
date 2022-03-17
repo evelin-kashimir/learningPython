9029267aca14c9b38124bf1dc26a7b5aa3bc5377
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-03-17 20:58:35 -0300

    Fundamentos: Importando métodos e funções externas

---
+++ PY1/aula04/imports.py
@@ -0,0 +1,13 @@
+import time
+import os
+from subRotinas import soma
+ #importando o arquivo que contem as rotinas
+
+soma(1, 3)
+
+print('Isso vai ser apagado...')
+time.sleep(3) #espera determinado tempo para executar algo
+os.system('cls') #digita algo no terminal, nesse caso limpando o terminal
+print('Teste do módulo')
+time.sleep(3)
+os.system('cls')
